6d16f342408460f40b0e58a2808d62f8f4f8fa6d
abidalikhatri771-hash <abidalikhatri771@gmail.com>
2026-09-14 17:24:00 +0500

    Fix hardcoded Windows path in generate-sitemap.js for GitHub Actions Linux runner

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const baseDir = 'c:\\Users\\Lapzone.pk\\Desktop\\website new';
+const baseDir = __dirname;
 const siteUrl = 'https://www.feeldesire.com';
 const currentDate = new Date().toISOString().split('T')[0];
 
